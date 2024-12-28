09e3629e44871547e36899e19524b873f46213c3
muhammadRifki2 <muhammadrifki48@gmail.com>
2024-12-28 16:00:32 +0700

    ubah judul form login

--- login.php
+++ login.php
@@ -76,7 +76,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <div class="card-body">
           <div class="text-center mb-3">
             <i class="bi bi-person-circle h1 display-4"></i>
-            <p>Genshin Impact</p>
+            <p>wiki Genshin Impact</p>
             <hr />
           </div>
           <form action="" method="post">
